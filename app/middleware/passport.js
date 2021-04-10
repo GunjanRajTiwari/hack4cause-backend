4449e5f6fce95d3fffd112e98a7dc4c549d2f25a
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-04-10 17:42:47 +0545

    passport middleware change

--- app/middleware/passport.js
+++ app/middleware/passport.js
@@ -1,48 +1,49 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-module.exports = function(passport) {
-    passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL
-    },
-    
-        async (accessToken, refreshToken, profile, done) => {
-            // console.log(profile);
-            const newUser = {
-                googleID: profile.id,
-                displayName: profile.displayName,
-                firstName: profile.givenName,
-                lastName: profile.familyName,
-                image: profile.photos[0].value,
-                email: profile.emails[0].value
+module.exports = function (passport) {
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                callbackURL: process.env.CALLBACK_URL,
+            },
 
-            }
+            async (accessToken, refreshToken, profile, done) => {
+                // console.log(profile);
+                const newUser = {
+                    googleID: profile.id,
+                    displayName: profile.displayName,
+                    firstName: profile.givenName,
+                    lastName: profile.familyName,
+                    image: profile.photos[0].value,
+                    email: profile.emails[0].value,
+                };
 
-            try {
-                let user = await User.findOne({ googleID: profile.id })
+                try {
+                    let user = await User.findOne({ googleID: profile.id });
 
-                if(user) {
-                    done(null, user)
-                }else{
-                    user = await User.create(newUser)
-                    done(null, newUser)
+                    if (user) {
+                        done(null, user);
+                    } else {
+                        user = await User.create(newUser);
+                        done(null, newUser);
+                    }
+                } catch (error) {
+                    console.error(error);
                 }
-            } catch (error) {
-                console.error(error);
             }
-        }
-    )
-    )
+        )
+    );
 
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    })
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
 
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user));
-    })
-}
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
+};
